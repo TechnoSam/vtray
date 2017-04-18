12045e55ca156796974b30d36d71943f4799c622
TechnoSam <technosam4jc@gmail.com>
2017-04-18 17:43:51 -0400

    Implement Color module
    All tests pass

--- color.cpp
+++ color.cpp
@@ -4,104 +4,124 @@
 
 #include "color.hpp"
 
+#include <math.h>
+
 Color::Color() {
 
-
+	r = 0;
+	g = 0;
+	b = 0;
 
 }
 
 Color::Color(double rCon, double gCon, double bCon) {
 
-
+	r = rCon;
+	g = gCon;
+	b = bCon;
 
 }
 
 void Color::setRed(double red) {
 
-
+	r = red;
 
 }
 
 void Color::setGreen(double green) {
 
-
+	g = green;
 
 }
 
 void Color::setBlue(double blue) {
 
-
+	b = blue;
 
 }
 
 double Color::getRed() const {
 
-	return 0;
+	return r;
 
 }
 
 double Color::getGreen() const {
 
-	return 0;
+	return g;
 
 }
 
 double Color::getBlue() const {
 
-	return 0;
+	return b;
 
 }
 
 void Color::scale(int max, const Color &maxColor) {
 
-
+	r = round((max * r) / maxColor.getRed());
+	g = round((max * g) / maxColor.getGreen());
+	b = round((max * b) / maxColor.getBlue());
 
 }
 
 Color& Color::operator+=(const Color &rhs) {
 
+	r += rhs.getRed();
+	g += rhs.getGreen();
+	b += rhs.getBlue();
 	return *this;
 
 }
 
 Color& Color::operator*=(int rhs) {
 
+	r *= rhs;
+	g *= rhs;
+	b *= rhs;
 	return *this;
 
 }
 
 Color& Color::operator*=(double rhs) {
 
+	r *= rhs;
+	g *= rhs;
+	b *= rhs;
 	return *this;
 
 }
 
 Color operator+(Color lhs, const Color &rhs) {
 
-	return Color();
+	lhs += rhs;
+	return lhs;
 
 }
 
 Color operator*(Color lhs, int rhs) {
 
-	return Color();
+	lhs *= rhs;
+	return lhs;
 
 }
 
 Color operator*(int lhs, const Color &rhs) {
 
-	return Color();
+	return rhs * lhs;
 
 }
 
 Color operator*(Color lhs, double rhs) {
 
-	return Color();
+	lhs *= rhs;
+	return lhs;
 
 }
 
 Color operator*(double lhs, const Color &rhs) {
 
-	return Color();
+	return rhs * lhs;
 
 }
