1310fdca2472d8bda6e3e35e96eba17ad0406531
TechnoSam <technosam4jc@gmail.com>
2017-04-20 03:46:28 -0400

    Add Object definition

--- object.cpp
+++ object.cpp
@@ -1,3 +1,23 @@
 // File: object.cpp
 // Author: Samuel McFalls
 // Description: Implements the Object class
+
+#include "object.hpp"
+
+Object::Object() {
+
+
+
+}
+
+Color Object::getColor() {
+
+
+
+}
+
+double Object::getLambert() {
+
+
+
+}
--- object.hpp
+++ object.hpp
@@ -1,3 +1,35 @@
 // File: object.hpp
 // Author: Samuel McFalls
 // Description: Defines the Object class
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "color.hpp"
+#include "vec3.hpp"
+#include "ray.hpp"
+
+class Object {
+
+public:
+
+	Object();
+
+	virtual Color getColor();
+
+	virtual double getLambert();
+
+	// Checks if the object is intersected by a ray
+	// @param ray The ray to check for intersection with
+	// @return A Vec3 with the position of intersection, 
+	//         or (inf, inf, inf) if no intersetion
+	virtual Vec3 intersectedBy(Ray ray) = 0;
+
+protected:
+
+	Color color;
+	double lambert;
+
+};
+
+#endif
