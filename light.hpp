f18dd1dc769248b2a6cd0de518730d1b1ddcb742
TechnoSam <technosam4jc@gmail.com>
2017-04-20 13:50:42 -0400

    Define Light module

--- light.hpp
+++ light.hpp
@@ -1,3 +1,33 @@
 // File: light.hpp
 // Author: Samuel McFalls
 // Description: Defines the Light class
+
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include "vec3.hpp"
+
+class Light {
+
+public:
+
+	Light();
+
+	Light(Vec3 locationCon, double intensityCon);
+
+	void setLocation(Vec3 locationSet);
+
+	void setIntensity(double intensitySet);
+
+	Vec3 getLocation();
+
+	double getIntensity();
+
+private:
+
+	Vec3 location;
+	double intensity;
+
+};
+
+#endif
