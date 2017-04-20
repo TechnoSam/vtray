df4c29facd0b7527d60b0d16fdab58758b1434bf
TechnoSam <technosam4jc@gmail.com>
2017-04-20 12:47:23 -0400

    Define Plane module

--- plane.hpp
+++ plane.hpp
@@ -1,3 +1,26 @@
 // File: plane.hpp
 // Author: Samuel McFalls
 // Description: Defines the Plane class
+
+#include "object.hpp"
+
+class Plane : public Object {
+
+public:
+
+	Plane();
+
+	Plane(Color colorCon, double lambertCon, Vec3 centerCon, double radiusCon);
+
+	Vec3 getNormal();
+
+	Vec3 getCenter();
+
+	Vec3 intersectedBy(Ray ray);
+
+private:
+
+	Vec3 normal;
+	Vec3 center;
+
+};
