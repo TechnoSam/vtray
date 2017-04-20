94e97497c38326bb99bba6bf67f5623fde9c9b91
TechnoSam <technosam4jc@gmail.com>
2017-04-20 02:58:21 -0400

    Define Ray module

--- ray.cpp
+++ ray.cpp
@@ -1,3 +1,35 @@
 // File: ray.cpp
 // Author: Samuel McFalls
 // Description: Implements the Ray class
+
+#include "ray.hpp"
+
+Ray::Ray() {
+
+
+
+}
+
+Ray::Ray(const Vec3 &origCon, const Vec3 &destCon) {
+
+
+
+}
+
+Vec3 Ray::getOrigin() {
+
+
+
+}
+
+Vec3 Ray::getDestination() {
+
+
+
+}
+
+Vec3 Ray::getDirection() {
+
+
+
+}
--- ray.hpp
+++ ray.hpp
@@ -1,3 +1,32 @@
 // File: ray.hpp
 // Author: Samuel McFalls
 // Description: Defines the Ray class
+
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.hpp"
+
+class Ray {
+
+public:
+
+	Ray();
+
+	Ray(const Vec3 &origCon, const Vec3 &destCon);
+
+	Vec3 getOrigin();
+
+	Vec3 getDestination();
+
+	Vec3 getDirection();
+
+private:
+
+	Vec3 origin;
+	Vec3 destination;
+	Vec3 direction;
+
+};
+
+#endif
