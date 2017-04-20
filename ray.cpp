395840c9f788e6bf21e36136b0d5241f0ddd3b02
TechnoSam <technosam4jc@gmail.com>
2017-04-20 03:31:30 -0400

    Implement Ray module
    Also had to fix the tests again. Can't seem to call methods on in-place
    constructed Vec3's, I'm not sure why this is...

--- ray.cpp
+++ ray.cpp
@@ -6,30 +6,34 @@
 
 Ray::Ray() {
 
-
+	origin = Vec3(0, 0, 0);
+	destination = Vec3(0, 0, 0);
+	direction = Vec3(0, 0, 0);
 
 }
 
 Ray::Ray(const Vec3 &origCon, const Vec3 &destCon) {
 
-
+	origin = origCon;
+	destination = destCon;
+	direction = (destination - origin).normalize();
 
 }
 
 Vec3 Ray::getOrigin() {
 
-
+	return origin;
 
 }
 
 Vec3 Ray::getDestination() {
 
-
+	return destination;
 
 }
 
 Vec3 Ray::getDirection() {
 
-
+	return direction;
 
 }
--- test_ray.cpp
+++ test_ray.cpp
@@ -10,13 +10,20 @@ TEST_CASE("Tests everything about rays", "[ray]") {
 	Ray ray0 = Ray();
 	Ray ray1 = Ray(Vec3(1, 1, 1), Vec3(5, 5, 5));
 
-	REQUIRE(ray0.getOrigin() == Vec3(0, 0, 0));
-	REQUIRE(ray0.getDestination() == Vec3(0, 0, 0));
-	REQUIRE(ray0.getDirection() == Vec3(0, 0, 0));
+	Vec3 test = ray0.getOrigin();
+	REQUIRE(test == Vec3(0, 0, 0));
+	test = ray0.getDestination();
+	REQUIRE(test == Vec3(0, 0, 0));
+	test = ray0.getDirection();
+	REQUIRE(test == Vec3(0, 0, 0));
 
-	REQUIRE(ray1.getOrigin() == Vec3(1, 1, 1));
-	REQUIRE(ray1.getDestination() == Vec3(5, 5, 5));
-	REQUIRE(ray1.getDirection() == Vec3(4, 4, 4).normalize());
-	REQUIRE(ray1.getDirection().magnitude() == 1);
+	test = ray1.getOrigin();
+	REQUIRE(test == Vec3(1, 1, 1));
+	test = ray1.getDestination();
+	REQUIRE(test == Vec3(5, 5, 5));
+	test = ray1.getDirection();
+	Vec3 testN = Vec3(4, 4, 4).normalize();
+	REQUIRE(test == testN);
+	REQUIRE(test.magnitude() == 1);
 
 }
