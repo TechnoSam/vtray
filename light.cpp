80aacc25decfc94d1bde9637b2a78e7ab5d7b994
TechnoSam <technosam4jc@gmail.com>
2017-04-20 13:58:38 -0400

    Implement Light module

--- light.cpp
+++ light.cpp
@@ -1,3 +1,42 @@
 // File: light.cpp
 // Author: Samuel McFalls
 // Description: Implements the Light class
+
+#include "light.hpp"
+
+Light::Light() {
+
+	location = Vec3();
+	intensity = 0;
+}
+
+Light::Light(Vec3 locationCon, double intensityCon) {
+
+	location = locationCon;
+	intensity = intensityCon;
+
+}
+
+void Light::setLocation(Vec3 locationSet) {
+
+	location = locationSet;
+
+}
+
+void Light::setIntensity(double intensitySet) {
+
+	intensity = intensitySet;
+
+}
+
+Vec3 Light::getLocation() {
+
+	return location;
+
+}
+
+double Light::getIntensity() {
+
+	return intensity;
+
+}
