15db9b7df7db7618ecdfb9b335fea8005c2ff370
TechnoSam <technosam4jc@gmail.com>
2017-04-20 04:02:35 -0400

    Add Sphere definition

--- sphere.cpp
+++ sphere.cpp
@@ -1,3 +1,11 @@
 // File: sphere.cpp
 // Author: Samuel McFalls
 // Description: Implements the Sphere class
+
+#include "sphere.hpp"
+
+Vec3 Sphere::intersectedBy(Ray ray) {
+
+
+
+}
--- sphere.hpp
+++ sphere.hpp
@@ -1,3 +1,23 @@
 // File: sphere.hpp
 // Author: Samuel McFalls
 // Description: Defines the Sphere class
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "object.hpp"
+
+class Sphere : public Object {
+
+public:
+
+	Vec3 intersectedBy(Ray ray);
+
+private:
+
+	Vec3 center;
+	double radius;
+
+};
+
+#endif
