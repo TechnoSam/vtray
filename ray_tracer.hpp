91ddf2b15f083be0fd16db70c2fcf438f062897e
TechnoSam <technosam4jc@gmail.com>
2017-04-22 14:40:37 -0400

    Define RayTracer module
    Threads can use the renderChunk method to push an image chunk to the
    output queue.

--- ray_tracer.hpp
+++ ray_tracer.hpp
@@ -1,3 +1,36 @@
 // File: ray_tracer.hpp
 // Author: Samuel McFalls
 // Description: Defines the RayTracer class
+
+#ifndef RAY_TRACER_H
+#define RAY_TRACER_H
+
+#include "scene.hpp"
+
+class RayTracer {
+
+public:
+
+	typedef struct {
+		int offsetX;
+		int offsetY;
+		Color max;
+		std::vector<std::vector<Color> > image;
+	} ImageChunk;
+
+	RayTracer();
+
+	RayTracer(Scene sceneCon);
+
+	void setScene(Scene sceneSet);
+
+	Scene getScene();
+
+	ImageChunk renderChunk(int startX, int endX, int startY, int endY);
+
+private:
+
+	Scene scene;
+};
+
+#endif
